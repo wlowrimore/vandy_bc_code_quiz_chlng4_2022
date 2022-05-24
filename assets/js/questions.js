126b91219ea1cca7ddb9f91edc53449912d08fea
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-05-24 09:57:37 -0500

    adds questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+let questionsArray = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alertts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "square brackets", "parentheses"],
+    answer: "parntheses",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store_____",
+    choices: [
+      "numbers and strings",
+      "booleans",
+      "other arrays",
+      "all of the above.",
+    ],
+    answer: "All of the above",
+  },
+  {
+    title:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "parentheses",
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
